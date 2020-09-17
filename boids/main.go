08c046b5f4a1c5e6e717a04c294152367acf0cf3
Katherine Ebel <kathyebel@me.com>
2020-09-17 14:40:13 -0400

    create boids and start their go routines

---
+++ boids/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"image/color"
+	"log"
+	"sync"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+const (
+	screenWidth, screenHeight = 640, 360
+	boidCount                 = 500
+	viewRadius                = 13
+	adjRate                   = 0.015
+)
+
+var (
+	green = color.RGBA{
+		R: 10,
+		G: 255,
+		B: 50,
+		A: 255,
+	}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
+	rWlock  = sync.RWMutex{}
+)
+
+func update(screen *ebiten.Image) error {
+	if !ebiten.IsDrawingSkipped() {
+		for _, boid := range boids {
+			screen.Set(int(boid.position.x+1), int(boid.position.y), green)
+			screen.Set(int(boid.position.x-1), int(boid.position.y), green)
+			screen.Set(int(boid.position.x), int(boid.position.y-1), green)
+			screen.Set(int(boid.position.x), int(boid.position.y+1), green)
+		}
+	}
+	return nil
+}
+func main() {
+	for i := 0; i < boidCount; i++ {
+		createBoid(i)
+	}
+	if err := ebiten.Run(update, screenWidth, screenHeight, 2, "Boids in a Box"); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module multithreading
 
 go 1.15
+
+require github.com/hajimehoshi/ebiten v1.11.8
